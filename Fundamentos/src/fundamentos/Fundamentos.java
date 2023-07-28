97279dda1d26e2df80223b77cd1aced4794597a6
Isabela <isabela.figueiredo@aluno.senai.br>
2023-07-28 11:06:25 -0300

    Entendendo Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -17,6 +17,11 @@ public class Fundamentos {
         double media = 5.5;
         
         String bairro = new String("Centro");
+        
+        // Declaração de Wrappers 
+        int nota1 = Integer.parseInt("8");
+        double mediaF = Double.parseDouble("5.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
