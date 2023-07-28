ebb456732e0fac67e5f879c977e6bb03bfe21114
Isabela <isabela.figueiredo@aluno.senai.br>
2023-07-28 10:49:02 -0300

    Criando variaveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -4,17 +4,17 @@
  */
 package fundamentos;
 
-/**
- *
- * @author Aluno
- */
 public class Fundamentos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Declaração de variaveis
+        String nome = "Moises";
+        int idade = 40;
+        boolean estudando = true;
+        double media = 5.5;
     }
     
 }
